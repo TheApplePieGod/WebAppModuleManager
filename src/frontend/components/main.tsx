a1bf73aaa76e527987c2c3dda2fe59fa4e2af920
TheApplePieGod <fishplaysgame@gmail.com>
2021-07-20 23:02:21 -0500

    Bug fixes & ItemGroup include changes

--- src/frontend/components/main.tsx
+++ src/frontend/components/main.tsx
@@ -42,8 +42,23 @@ export const Main = () => {
                             <Typography color="textPrimary"><b>Some setup requirements</b></Typography>
                             <Typography color="textPrimary">- The project must be a DotNet Core application</Typography>
                             <Typography color="textPrimary">- It is recommended to use dbup or some other database migration framework</Typography>
-                            <Typography color="textPrimary">- The WAMM Migrations folder must be included as an embedded resource in the .csproj file ({`<EmbeddedResource Include="WAMM\\Migrations\\*.sql" />`})</Typography>
+                            <Typography color="textPrimary">- Include these ItemGroups in your .csproj file ({`
+                            <ItemGroup>
+                                <EmbeddedResource Remove="WAMM\**" />
+                                <None Remove="WAMM\**" />
+                                <Content Remove="WAMM\**" />
+                                <Compile Remove="WAMM\**" />
+                                <TypeScriptCompile Remove="WAMM\**" />
+                            </ItemGroup>
+                        
+                            <ItemGroup>
+                                <EmbeddedResource Include="WAMM\Migrations\*.sql" />
+                                <Compile Include="WAMM\**\*.cs" />
+                            </ItemGroup>
+                            `
+                            })</Typography>
                             <Typography color="textPrimary">- You may have to specify the WAMM folder generated in your project's directory in your webpack config</Typography>
+                            <Typography color="textPrimary">- Visual Studio might lag when installing/removing big modules, so it might be best to close it</Typography>
                         </div>
                         <Button variant="contained" onClick={() => setInfoOpen(false)}>Close</Button>
                     </React.Fragment>
